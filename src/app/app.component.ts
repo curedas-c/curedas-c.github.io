f451ad40dd13dd35b406f6476395c7a082f00716
curedas <codjocuredas@gmail.com>
2021-05-05 22:29:21 +0000

    feat: adds preloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import Typewriter from 'typewriter-effect/dist/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, AfterViewInit {
   writingText: any;
@@ -19,19 +19,30 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
+    this.disableLoadingScreen();
     this.initTypeWriter();
   }
 
   initTypeWriter() {
     this.typeWriter
-    .typeString('donne vie à vos projets!')
-    .pauseFor(2000)
-    .deleteAll()
-    .typeString('construit des interfaces modernes!')
-    .pauseFor(2000)
-    .deleteAll()
-    .typeString('<span class="red-color">❤</span> la qualité!')
-    .pauseFor(2500)
-    .start();
+      .typeString('donne vie à vos projets!')
+      .pauseFor(2000)
+      .deleteAll()
+      .typeString('construit des interfaces modernes!')
+      .pauseFor(2000)
+      .deleteAll()
+      .typeString('<span class="red-color">❤</span> la qualité!')
+      .pauseFor(2500)
+      .start();
+  }
+
+  disableLoadingScreen() {
+    let preloader = document.querySelector('#loader-wrapper') as HTMLDivElement;
+    preloader.style.animation = 'fade-disappear .8s linear forwards';
+    setTimeout(() => {
+      requestAnimationFrame(() => {
+        preloader.style.display = 'none';
+      });
+    }, 1000);
   }
 }
