3c27afd4734e3216a6b8c10a113240e7def67c98
curedas <codjocuredas@gmail.com>
2021-05-05 20:16:19 +0000

    feat: integrates main text  section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import Typewriter from 'typewriter-effect/dist/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit, AfterViewInit {
+  writingText: any;
+  typeWriter: any;
+
+  ngOnInit(): void {
+    this.writingText = document.querySelector('.main-text h2 span');
+    this.typeWriter = new Typewriter(this.writingText, {
+      loop: true,
+      delay: 75,
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.initTypeWriter();
+  }
+
+  initTypeWriter() {
+    this.typeWriter
+    .typeString('donne vie à vos projets!')
+    .pauseFor(2000)
+    .deleteAll()
+    .typeString('construit des interfaces modernes!')
+    .pauseFor(2000)
+    .deleteAll()
+    .typeString('<span class="red-color">❤</span> la qualité!')
+    .pauseFor(2500)
+    .start();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgParticlesModule } from 'ng-particles';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    NgParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
